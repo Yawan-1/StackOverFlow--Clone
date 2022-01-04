f7ae68154166b40c480caaf988add105ae5d4ac9
Space <yadavyavan17586@gmail.com>
2022-01-05 02:30:22 +0530

    migrated commit

--- main/settings.py
+++ main/settings.py
@@ -125,23 +125,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 If you don't want to use postgresql then remove comment of sqlite's configuration and
 comment in the postgresql configuration
 """
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'POSTGRESQL_NAME',
-        'USER': 'POSTGRESQL_USER',
-        'PASSWORD': 'POSTGRESQL_PASSWORD',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'POSTGRESQL_NAME',
+#         'USER': 'POSTGRESQL_USER',
+#         'PASSWORD': 'POSTGRESQL_PASSWORD',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 
