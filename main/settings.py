bdb7f23ed23639d46f9fdca6418d6ecf36877943
Space <yadavyavan17586@gmail.com>
2022-01-05 09:58:06 +0530

    Added staticsilfes dir 2

--- main/settings.py
+++ main/settings.py
@@ -196,23 +196,21 @@ LOGIN_URL = 'users:login_request'
 #     os.path.join(BASE_DIR, 'media'),
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# TEMPLATE_DIRS = (
-#     os.path.join(BASE_DIR,  'templates'),
-#     # Add to this list all the locations containing your static files 
-# )
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
