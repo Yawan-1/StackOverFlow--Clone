7f38184c994cf8ba4f8c4e9fcf28ed7087886ef1
Space <yadavyavan17586@gmail.com>
2022-01-05 13:57:35 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -209,7 +209,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# rofr
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
