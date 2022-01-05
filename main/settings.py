b20551e326068eeae8c29457b79764b68071178d
Space <yadavyavan17586@gmail.com>
2022-01-05 13:47:53 +0530

    Set Debug=False 6

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,7 @@ settings.configure()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://stackoverflow---clone.herokuapp.com']
+ALLOWED_HOSTS = ['https://stackoverflow---clone.herokuapp.com','.herokuapp.com']
 
 # Application definition
 
