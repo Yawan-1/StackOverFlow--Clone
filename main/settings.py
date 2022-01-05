c9e5ad223d19deb237c76d0b64a1c1fcf0f24abc
Space <yadavyavan17586@gmail.com>
2022-01-05 09:59:10 +0530

    Added staticsilfes dir 3

--- main/settings.py
+++ main/settings.py
@@ -198,15 +198,16 @@ LOGIN_URL = 'users:login_request'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
