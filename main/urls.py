1488462b9020e113da12b153b9d3d6e00e0ece28
Space <yadavyavan17586@gmail.com>
2022-01-08 20:10:38 +0530

    Updated Logo 2

--- main/urls.py
+++ main/urls.py
@@ -22,9 +22,9 @@ from qa import views as qa_views
 
 handler404 = qa_views.handler404
 handler500 = qa_views.handler500
-# 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminisGoodofmy_clone_so/', admin.site.urls),
     path('', include('profile.urls')),
     path('', include('qa.urls')),
     path('', include('notification.urls')),
