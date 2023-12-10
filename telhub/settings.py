d88e8ecad7d83b3a1881a1dd21146e9373895ee8
João Pedro Pereira <joaopedrosp6@gmail.com>
2023-12-10 11:21:50 -0300

    triste

--- telhub/settings.py
+++ telhub/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "telhub.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
