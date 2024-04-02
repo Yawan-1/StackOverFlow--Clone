f971a66c133d9ec47dde606c51b7aa88b07d876f
Rabbani2101 <156078537+Rabbani2101@users.noreply.github.com>
2024-04-02 17:40:47 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -128,23 +128,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 If you don't want to use postgresql then remove comment of sqlite's configuration and
 comment in the postgresql configuration
 """
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'POSTGRESQL_NAME',
-#         'USER': 'POSTGRESQL_USER',
-#         'PASSWORD': 'POSTGRESQL_PASSWORD',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'django-server',
+         'USER': 'dbuser',
+         'PASSWORD': 'Rabbani@123',
+         'HOST': 'django-server.mysql.database.azure.com',
+         'PORT': '3306',
+     }
+ }
 
 # Password validation
 
